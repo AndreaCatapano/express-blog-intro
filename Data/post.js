74261cff07faf867de7b6ead66e132244f1bfe4d
AndreaCatapano <a.catapano96@gmail.com>
2025-03-26 15:15:19 +0100

    feat: added array of object and export in app.js

---
+++ Data/post.js
@@ -0,0 +1,35 @@
+post = [
+    {
+        title: "Ciambellone",
+        text: "Prova",
+        img: "img/ciambellone.jpeg",
+        tags: ["tag1", "tag2", "tag3"]
+    },
+    {
+        title: "Ciambellone",
+        text: "Prova",
+        img: "img/cracker_barbabietola.jpeg",
+        tags: ["tag1", "tag2", "tag3"]
+    },
+    {
+        title: "Ciambellone",
+        text: "Prova",
+        img: "img/pane_fritto_dolce.jpeg",
+        tags: ["tag1", "tag2", "tag3"]
+    },
+    {
+        title: "Ciambellone",
+        text: "Prova",
+        img: "img/pasta_barbabietola.jpeg",
+        tags: ["tag1", "tag2", "tag3"]
+    },
+    {
+        title: "Torta Paesana",
+        text: "Prova",
+        img: "img/torta_paesana.jpeg",
+        tags: ["tag1", "tag2", "tag3"]
+    }
+]
+
+
+module.exports = post;
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const post = import(post);
 const app = express();
 const port = 3000; 
 
@@ -6,6 +7,5 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send("Server del mio blog");
 })
-
 
 app.listen(port, () => console.log("Example app listening on port:" + port))
