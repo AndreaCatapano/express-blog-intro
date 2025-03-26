f24211207a9de8413a81641abe83718a21218c52
AndreaCatapano <a.catapano96@gmail.com>
2025-03-26 15:32:19 +0100

    feat: added call for post and free use for public folder

--- Data/post.js
+++ Data/post.js
@@ -2,31 +2,31 @@ post = [
     {
         title: "Ciambellone",
         text: "Prova",
-        img: "img/ciambellone.jpeg",
+        img: "/img/ciambellone.jpeg",
         tags: ["tag1", "tag2", "tag3"]
     },
     {
         title: "Ciambellone",
         text: "Prova",
-        img: "img/cracker_barbabietola.jpeg",
+        img: "/img/cracker_barbabietola.jpeg",
         tags: ["tag1", "tag2", "tag3"]
     },
     {
         title: "Ciambellone",
         text: "Prova",
-        img: "img/pane_fritto_dolce.jpeg",
+        img: "/img/pane_fritto_dolce.jpeg",
         tags: ["tag1", "tag2", "tag3"]
     },
     {
         title: "Ciambellone",
         text: "Prova",
-        img: "img/pasta_barbabietola.jpeg",
+        img: "/img/pasta_barbabietola.jpeg",
         tags: ["tag1", "tag2", "tag3"]
     },
     {
         title: "Torta Paesana",
         text: "Prova",
-        img: "img/torta_paesana.jpeg",
+        img: "/img/torta_paesana.jpeg",
         tags: ["tag1", "tag2", "tag3"]
     }
 ]
--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const post = import(post);
+const post = require("./Data/post.js");
 const app = express();
 const port = 3000; 
 
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send("Server del mio blog");
 })
 
-app.listen(port, () => console.log("Example app listening on port:" + port))
+
+app.get('/bacheca', (req, res) => {
+    res.send(post);
+})
+
+app.listen(port, () => console.log("Example app listening on port:" + port));
